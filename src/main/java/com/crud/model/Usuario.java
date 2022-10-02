0318d5736c1221910445744aa8146e5a8eb99eaf
EmilioJSalazar <emilio.salazar@udla.edu.ec>
2022-10-01 19:17:52 -0500

    Creado el proyecto y el modelo de usuario

---
+++ src/main/java/com/crud/model/Usuario.java
@@ -0,0 +1,80 @@
+package com.crud.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String telefono;
+	private String contrasenia;
+	
+	public Usuario() {
+		super();
+	}
+	
+	public Usuario(Integer id, String nombre, String apellido, String correo, String telefono, String contrasenia) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.contrasenia = contrasenia;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo
+				+ ", telefono=" + telefono + ", contrasenia=" + contrasenia + "]";
+	}
+	
+	
+}
